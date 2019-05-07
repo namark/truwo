56313e78a4fd8b6326cc6087d0ac2d973b9f8cf8
namark <namark@disroot.org>
2019-05-08 03:48:17 +0400

    Plain button copy made explicit.

--- source/plain_button.h
+++ source/plain_button.h
@@ -3,6 +3,7 @@
 class plain_button : public button
 {
 	public:
+	explicit plain_button(const plain_button&) = default;
 	plain_button(graphical::color color, range2D bounds);
 	virtual void draw(const graphical::surface&) override;
 
